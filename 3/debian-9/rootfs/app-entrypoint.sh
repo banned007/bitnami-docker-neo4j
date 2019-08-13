c32965118f5dc7fd7c6d37d27994320836c35e94
banned007 <ps3.walter@hotmail.com>
2019-08-13 10:41:39 -0600

    fix on read-only file-share

--- 3/debian-9/rootfs/app-entrypoint.sh
+++ 3/debian-9/rootfs/app-entrypoint.sh
@@ -7,6 +7,12 @@ print_welcome_page
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
   nami_initialize neo4j
+  mkdir tmp/bitcopy
+  cp -r /bitnami/neo4j/data /tmp/bitcopy
+  rm -r /opt/bitnami/neo4j/data
+  cp -r /tmp/bitcopy/data /opt/bitnami/neo4j
+  chmod -R 777 /opt/bitnami/neo4j/data
+  rm -r /tmp/bitcopy
   info "Starting neo4j... "
 fi
 
